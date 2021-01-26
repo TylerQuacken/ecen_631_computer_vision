2383a2f49dd356dbd529008dffe2599d0b8fad4d
Tyler Quackenbush <tylerquacken@gmail.com>
2021-01-26 10:03:46 -0700

    Got thresholding implemented

--- openCV_practice/scripts/detect_features.py
+++ openCV_practice/scripts/detect_features.py
@@ -1,21 +1,35 @@
-import cv2
+import cv2 as cv 
 
-camera = cv2.VideoCapture(0)
-width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-vout = cv2.VideoWriter('./video.avi', cv2.VideoWriter_fourcc(*'XVID'), 25, (width, height))
+camera = cv.VideoCapture(0)
+width = int(camera.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(camera.get(cv.CAP_PROP_FRAME_HEIGHT))
+vout = cv.VideoWriter('./video.avi', cv.VideoWriter_fourcc(*'XVID'), 25, (width, height))
 
 inputCounter = 0
 methods = ["binarized", "canny", "corners", "lines", "difference"]
+numMethods = len(methods)
 
 while True:
     ret0, frame = camera.read()
-    cv2.imshow("Cam 0", frame)
-    vout.write(frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+    if inputCounter == 1:
+        grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        retProcessed, processedImage = cv.threshold(grey,110,255,cv.THRESH_BINARY)
+
+    else:
+        processedImage = frame
+        retProcessed = ret0
+        
+    cv.imshow("Cam 0", processedImage)
+    vout.write(processedImage)
+    
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        inputCounter += 1
+
+        if inputCounter > numMethods:
+            break
 
 # When everything is done, release the capture
-cv2.imwrite('./Cam0.jpg', frame)
+cv.imwrite('./Cam0.jpg', processedImage)
 camera.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
