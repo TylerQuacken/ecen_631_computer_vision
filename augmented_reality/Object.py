2e40154a7d63f03565b454593ac58705b2cfe115
Tyler Quackenbush <tylerquacken@gmail.com>
2021-02-24 17:09:09 -0700

    Refactored code, added attributes to object

--- augmented_reality/Object.py
+++ augmented_reality/Object.py
@@ -1,11 +1,16 @@
 import numpy as np
+import cv2
 
 
 class Object():
     def __init__(self):
         self.location = np.zeros([3])
+        self.mass = 1.0
+        self.velocity = np.zeros([3])
+        self.xRange = np.array([0., 11.])
+        self.yRange = np.array([-8., 0.])
 
-    def update(self):
+    def update(self, RVec):
         pass
 
     def get_points(self):
@@ -22,3 +27,14 @@ class Object():
         points[:, 0:3, 0] = points[:, 0:3, 0] + self.location
 
         return points[:, 0:3, 0].T
+
+    def draw_object_on_image(self, image, camera, RVec, T):
+        projPoints, _ = cv2.projectPoints(self.get_points(), RVec, T,
+                                          camera.cameraMatrix,
+                                          camera.distortion)
+        imgPoints = projPoints.squeeze().astype('int')
+
+        for i in range(len(imgPoints)):
+            cv2.circle(image, tuple(imgPoints[i, :]), 10, (0, 0, 255), -1)
+
+        return image
--- augmented_reality/balance_object_from_webcam.py
+++ augmented_reality/balance_object_from_webcam.py
@@ -14,9 +14,8 @@ chessPoints = np.zeros((chessXPoints * chessYPoints, 3), np.float32)
 chessPoints[:, :2] = np.mgrid[:chessXPoints, :chessYPoints].T.reshape(-1, 2)
 
 object3d = Object()
-arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 arucoParams = cv2.aruco.DetectorParameters_create()
-arucoID = 211
 arucoPoints = np.array([[0, 0, 0], [7.25, 0, 0], [-0.06, -4.875, 0],
                         [17.1250, -4.8, 0]])
 
@@ -41,8 +40,9 @@ def find_chessboard_corners(image):
 
 
 def find_aruco(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cornersDetected, ids, rejected = cv2.aruco.detectMarkers(
-        image, arucoDict, parameters=arucoParams)
+        gray, arucoDict, parameters=arucoParams)
 
     if len(cornersDetected) < 4:
         ret = False
@@ -58,44 +58,44 @@ def find_aruco(image):
     return ret, corners, ids
 
 
+def get_R_T_from_aruco(corners):
+    RVecAruco, TVecs, _ = cv2.aruco.estimatePoseSingleMarkers(
+        corners, 2, camera.cameraMatrix, camera.distortion)
+
+    TVecs = TVecs.squeeze()
+    R01 = TVecs[1, :] - TVecs[0, :]
+    R03 = TVecs[3, :] - TVecs[0, :]
+    zVec = np.cross(R03, R01)
+    zVec = zVec / np.linalg.norm(zVec)
+    xVec = R01 / np.linalg.norm(R01)
+    yVec = np.cross(zVec, xVec)
+
+    R = np.zeros([3, 3])
+    R[:, 0] = xVec
+    R[:, 1] = yVec
+    R[:, 2] = zVec
+
+    RVec, _ = cv2.Rodrigues(R)
+    T = TVecs[0, :]
+
+    # for i in range(TVecs.shape[0]):
+    #     cv2.aruco.drawAxis(image, camera.cameraMatrix, camera.distortion,
+    #                        RVecAruco[i, :, :], TVecs[i, :], 0.7)
+    return RVec, T
+
+
 while True:
 
     image = camera.get_image()
     ret, corners, ids = find_aruco(image)
+    # ret, corners = find_chessboard_corners(image)
 
     if ret:
-        # ret, RVec, T = cv2.solvePnP(arucoPoints, corners, camera.cameraMatrix,
+        # ret, RVec, T = cv2.solvePnP(chessPoints, corners, camera.cameraMatrix,
         #                             camera.distortion)
-        RVecAruco, TVecs, _ = cv2.aruco.estimatePoseSingleMarkers(
-            corners, 5, camera.cameraMatrix, camera.distortion)
+        RVec, T = get_R_T_from_aruco(corners)
 
-        TVecs = TVecs.squeeze()
-        R01 = TVecs[1, :] - TVecs[0, :]
-        R03 = TVecs[3, :] - TVecs[0, :]
-        zVec = np.cross(R03, R01)
-        zVec = zVec / np.linalg.norm(zVec)
-        xVec = R01 / np.linalg.norm(R01)
-        yVec = np.cross(zVec, xVec)
-
-        R = np.zeros([3, 3])
-        R[:, 0] = xVec
-        R[:, 1] = yVec
-        R[:, 2] = zVec
-
-        RVec, _ = cv2.Rodrigues(R)
-        T = TVecs[0, :]
-
-        # for i in range(TVecs.shape[0]):
-        #     cv2.aruco.drawAxis(image, camera.cameraMatrix, camera.distortion,
-        #                        RVecAruco[i, :, :], TVecs[i, :], 0.7)
-
-        projPoints, _ = cv2.projectPoints(object3d.get_points(), RVec, T,
-                                          camera.cameraMatrix,
-                                          camera.distortion)
-        imgPoints = projPoints.squeeze().astype('int')
-
-        for i in range(len(imgPoints)):
-            cv2.circle(image, tuple(imgPoints[i, :]), 10, (0, 0, 255), -1)
+        image = object3d.draw_object_on_image(image, camera, RVec, T)
 
     flipped = np.flip(image, axis=1)
     cv2.imshow("camera", flipped)
