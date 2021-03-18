ad81ab8e2cd69711fe5216b81a8547861dc8250d
Tyler Quackenbush <tylerquacken@gmail.com>
2021-03-18 17:10:25 -0600

    Finished!

--- augmented_reality_stereo/Object.py
+++ augmented_reality_stereo/Object.py
@@ -8,20 +8,37 @@ class Object():
         self.position = np.zeros([3])
         self.mass = 1.0
         self.velocity = np.zeros([3])
-        self.positionMax = np.array([0., 4., 0.])
-        self.positionMin = np.array([-8., -1., 0.])
+        self.positionMax = np.array([8.5, 0.5, 0.])
+        self.positionMin = np.array([-0.5, -6., 0.])
         self.groundR = np.zeros([3, 3])
 
     def set_ground(self, RVec):
         self.groundR, _ = cv2.Rodrigues(RVec)
 
-    def update(self, RVec, dt):
+    def update(self, RVec, T, puckCamera, dt):
         R, _ = cv2.Rodrigues(RVec)
         gVecWorld = 1.5 * np.array([0., 0., -9.8])
         gVecCamera = self.groundR.T @ gVecWorld
         gVecPlane = R @ gVecCamera
 
-        accel = gVecPlane * np.array([1., -1., 0.])
+        accel = gVecPlane * np.array([-1., 1., 0.])
+
+        if puckCamera is not None:
+
+            puckPlane = R @ (puckCamera - T) / 25.4
+            puckPlane[2] = 0.0
+
+            QP = (puckPlane - self.position)[:2]
+            r = np.linalg.norm(QP)
+            if r < .5:
+                r = 0.5
+            # print(puckPlane, self.position)
+            # if np.linalg.norm(QP) < 2.25:
+            #     print("Hit")
+            #     self.velocity -= 1.5 * np.dot(self.velocity, QP)
+            G = 30.0
+            accel[:2] += G / r**2 * QP / r
+
         self.velocity += accel * dt
         self.position += self.velocity * dt
         tooHigh = np.where(self.position > self.positionMax)
@@ -33,35 +50,58 @@ class Object():
 
     def get_points(self):
         # cube points
-        points = np.zeros([3, 8])
-        points[:, 0] = [0, 0, 0]
-        points[:, 1] = [0, 2, 0]
-        points[:, 2] = [2, 2, 0]
-        points[:, 3] = [2, 0, 0]
-        points[:, 4] = [0, 0, 1]
-        points[:, 5] = [0, 2, 1]
-        points[:, 6] = [2, 2, 1]
-        points[:, 7] = [2, 0, 1]
+        # points = np.zeros([3, 8])
+        # points[:, 0] = [-1, -1, 0]
+        # points[:, 1] = [-1, 1, 0]
+        # points[:, 2] = [1, 1, 0]
+        # points[:, 3] = [1, -1, 0]
+        # points[:, 4] = [-1, -1, 1]
+        # points[:, 5] = [-1, 1, 1]
+        # points[:, 6] = [1, 1, 1]
+        # points[:, 7] = [1, -1, 1]
+        # points = points + np.expand_dims(self.position, axis=1)
+
+        # lines = np.array([[0, 1], [0, 4], [0, 3], [1, 2], [1, 5], [2, 6],
+        #                   [2, 3], [3, 7], [4, 5], [4, 7], [5, 6], [6, 7]])
+
+        nPoints = 20
+        points = np.zeros([3, nPoints * 2])
+        for i in range(nPoints):
+            theta = 2 * np.pi * i / nPoints
+            points[:, i] = [np.cos(theta), np.sin(theta), 0]
+            points[:, i + nPoints] = [np.cos(theta), np.sin(theta), 1]
+
         points = points + np.expand_dims(self.position, axis=1)
 
-        lines = np.array([[0, 1], [0, 4], [0, 3], [1, 2], [1, 5], [2, 6],
-                          [2, 3], [3, 7], [4, 5], [4, 7], [5, 6], [6, 7]])
+        lines = nPoints
 
         return points * 25, lines
 
     def draw_object_on_image(self, image, camera, RVec, T):
         points, lines = self.get_points()
-        projPoints, _ = cv2.projectPoints(points, RVec, T, camera.cameraMatrix,
-                                          camera.distortion)
+        projPoints, _ = cv2.projectPoints(points, RVec, T, camera.cameraL,
+                                          camera.distL)
         imgPoints = np.round_(projPoints.squeeze()).astype('int')
 
         # for i in range(len(imgPoints)):
         #     cv2.circle(image, tuple(imgPoints[i, :]), 10, (0, 0, 255), -1)
 
-        for i in range(len(lines)):
-            endPointNums = lines[i, :]
-            endPoints = imgPoints[endPointNums, :]
-            cv2.line(image, tuple(endPoints[0, :]), tuple(endPoints[1, :]),
-                     (0, 0, 255), 2)
+        if type(lines) != int:
+            for i in range(len(lines)):
+                endPointNums = lines[i, :]
+                endPoints = imgPoints[endPointNums, :]
+                cv2.line(image, tuple(endPoints[0, :]), tuple(endPoints[1, :]),
+                         (0, 0, 255), 2)
+        else:
+            for i in range(lines):
+                iPlus1 = (i + 1) % lines
+                cv2.line(image, tuple(imgPoints[i, :]),
+                         tuple(imgPoints[iPlus1, :]), (0, 0, 255), 2)
+
+                cv2.line(image, tuple(imgPoints[i + lines, :]),
+                         tuple(imgPoints[iPlus1 + lines, :]), (0, 0, 255), 2)
+
+                cv2.line(image, tuple(imgPoints[i, :]),
+                         tuple(imgPoints[i + lines, :]), (0, 0, 255), 2)
 
         return image
--- augmented_reality_stereo/balance_object_from_webcam.py
+++ augmented_reality_stereo/balance_object_from_webcam.py
@@ -3,11 +3,11 @@ import cv2
 import sys
 from Object import Object
 sys.path.append('..')
-from utils.LogitechWebcam import LogitechWebcam
+from utils.ZedCamera import ZedCamera
 import time
 from IPython import embed
 
-camera = LogitechWebcam()
+camera = ZedCamera()
 chessXPoints = 9
 chessYPoints = 7
 chessPoints = np.zeros((chessXPoints * chessYPoints, 3), np.float32)
@@ -17,41 +17,19 @@ chessPoints[:, :2] = 24.5 * np.mgrid[:chessXPoints, :chessYPoints].T.reshape(
 object3d = Object()
 arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 arucoParams = cv2.aruco.DetectorParameters_create()
+arucoParams.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
 arucoPoints = 25.4 * np.array([[0, 0], [7.25, 0], [-0.06, -4.875],
                                [17.1250, -4.8]])
-
-
-def find_chessboard_corners(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    ret, corners = cv2.findChessboardCorners(gray,
-                                             (chessXPoints, chessYPoints),
-                                             None)
-    # cornerImg = image
-
-    if ret:
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30,
-                    0.001)
-        corners = cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), criteria)
-        # cornerImg = cv2.drawChessboardCorners(image,
-        #                                       (chessXPoints, chessYPoints),
-        #                                       corners, ret)
-
-    return ret, corners
 
 
 def find_aruco(image, numNeeded=4):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cornersDetected, ids, rejected = cv2.aruco.detectMarkers(
         gray, arucoDict, parameters=arucoParams)
-
-    # embed()
-    corners = []
-    idDetected = []
+    centroids = np.zeros([4, 2])
 
     if len(cornersDetected) < numNeeded:
         ret = False
-        print(len(cornersDetected))
     else:
         for i in range(4):
             index = np.where(ids == i)[0]
@@ -59,23 +37,52 @@ def find_aruco(image, numNeeded=4):
                 index = None
             else:
                 index = index[0]
-                corners.append(cornersDetected[index])
-                idDetected.append(ids[index])
+                centroids[i, :] = get_centroid_from_corners(
+                    cornersDetected[index].squeeze())
+
         ret = True
 
-    if numNeeded == 1 and ret and idDetected[0] != 0:
-        ret = False
-
-    return ret, corners, idDetected
+    return ret, centroids
 
 
-def get_R_T_from_aruco(corners):
-    RVecAruco, TVecs, _ = cv2.aruco.estimatePoseSingleMarkers(
-        corners, 50, camera.cameraMatrix, camera.distortion)
+def get_centroid_from_corners(corners):
+    x0, y0 = tuple(corners[0, :])
+    x1, y1 = tuple(corners[1, :])
+    x2, y2 = tuple(corners[2, :])
+    x3, y3 = tuple(corners[3, :])
+    m02 = (y0 - y2) / (x0 - x2)
+    m13 = (y1 - y3) / (x1 - x3)
+    xc = (m02 * x0 - y0 - m13 * x1 + y1) / (m02 - m13)
+    yc = m02 * (xc - x0) + y0
+    centroid = np.array([xc, yc])
+    return centroid
 
-    TVecs = TVecs.squeeze()
-    R01 = TVecs[1, :] - TVecs[0, :]
-    R03 = TVecs[3, :] - TVecs[0, :]
+
+def get_R_T_from_aruco(centroidsL, centroidsR):
+    centroidLu = cv2.undistortPoints(centroidsL,
+                                     camera.cameraL,
+                                     camera.distL,
+                                     R=camera.R1,
+                                     P=camera.P1).squeeze()
+    centroidRu = cv2.undistortPoints(centroidsR,
+                                     camera.cameraR,
+                                     camera.distR,
+                                     R=camera.R2,
+                                     P=camera.P2).squeeze()
+
+    disparity = centroidLu[:, 0] - centroidRu[:, 0]
+
+    points2d = np.zeros([4, 1, 3])
+    points2d[:, 0, :2] = centroidLu
+    points2d[:, 0, 2] = disparity
+
+    points3d = cv2.perspectiveTransform(points2d, camera.Q).squeeze()
+
+    # points3d = points3d + (camera.T / 2).squeeze()
+
+    points3d = points3d.squeeze()
+    R01 = points3d[1, :] - points3d[0, :]
+    R03 = points3d[3, :] - points3d[0, :]
     zVec = np.cross(R03, R01)
     zVec = zVec / np.linalg.norm(zVec)
     xVec = R01 / np.linalg.norm(R01)
@@ -88,73 +95,123 @@ def get_R_T_from_aruco(corners):
     R[:, 2] = zVec
 
     RVec, _ = cv2.Rodrigues(R)
-    T = TVecs[0, :]
+    T = points3d[0, :]
 
-    for i in range(TVecs.shape[0]):
-        cv2.aruco.drawAxis(image, camera.cameraMatrix, camera.distortion,
-                           RVecAruco[i, :, :], TVecs[i, :], 0.7)
     return RVec, T
 
 
-def get_R_T_from_single_aruco(corners):
-    RVecAruco, TVecs, _ = cv2.aruco.estimatePoseSingleMarkers(
-        corners, 50, camera.cameraMatrix, camera.distortion)
+def locate_puck(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    T = TVecs[0, 0, :]
-    RVec = RVecAruco[0, 0, :]
-    return RVec, T
+    # lower_green = np.array([84, 40, 40]) * np.array(
+    #     [1 / 359, 1 / 100, 1 / 100]) * 255
+    # upper_green = np.array([150, 100, 100]) * np.array(
+    #     [1 / 359, 1 / 100, 1 / 100]) * 255
+    lower_green = np.array([35, 100, 10])
+    upper_green = np.array([100, 255, 120])
+
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+
+    # lower_white = np.array([0, 100, 100])
+    # upper_white = np.array([20, 255, 255])
+
+    # mask_white = cv2.inRange(hsv, lower_white, upper_white)
+
+    # mask = mask_green mask_white
+
+    mask = cv2.erode(mask, None, iterations=6)
+    mask = cv2.dilate(mask, None, iterations=6)
+
+    if np.any(mask):
+        M = cv2.moments(mask)
+        cX = int(M['m10'] / M['m00'])
+        cY = int(M['m01'] / M['m00'])
+        centroid = np.array([cX, cY])
+
+    else:
+        centroid = None
+    return centroid
 
 
-def get_homography_from_single_aruco(corners):
-    singleArucoPoints = np.array([[[1, -1], [-1, -1], [-1, 1], [1, 1]]])
-    graphy, _ = cv2.findHomography(singleArucoPoints, corners[0], cv2.RANSAC)
-    return graphy
+def puck_to_camera(centroidL, centroidR):
+    centroidLu = cv2.undistortPoints(centroidL.astype('float64'),
+                                     camera.cameraL,
+                                     camera.distL,
+                                     R=camera.R1,
+                                     P=camera.P1).squeeze()
+    centroidRu = cv2.undistortPoints(centroidR.astype('float64'),
+                                     camera.cameraR,
+                                     camera.distR,
+                                     R=camera.R2,
+                                     P=camera.P2).squeeze()
+
+    disparity = centroidLu[0] - centroidRu[0]
+
+    point2d = np.zeros([1, 1, 3])
+    point2d[0, 0, :2] = centroidLu
+    point2d[0, 0, 2] = disparity
+
+    point3d = cv2.perspectiveTransform(point2d, camera.Q).squeeze()
+
+    # points3d = points3d + (camera.T / 2).squeeze()
+
+    return point3d
 
 
 while True:
     # Calibrate ground
-    image = camera.get_image()
-    cv2.imshow("camera", image)
+    imageL, imageR = camera.get_images()
+    ret, centroids = find_aruco(imageL)
+    if ret:
+        for i in range(4):
+            cv2.circle(imageL, tuple(centroids[i, :].astype('int')), 2,
+                       (0, 0, 255), -1)
+
+    cv2.imshow("camera", imageL)
     if cv2.waitKey(1) & 0xFF == ord(' '):
-        calibImage = image
+        calibImageL = imageL
+        calibImageR = imageR
         break
 
-ret, corners, ids = find_aruco(calibImage, numNeeded=1)
-# RVec, T = get_R_T_from_aruco(corners)
-RVec, T = get_R_T_from_single_aruco(corners)
-# ret, corners = find_chessboard_corners(image)
-# ret, RVec, T = cv2.solvePnP(chessPoints, corners, camera.cameraMatrix,
-#                             camera.distortion)
+ret, centroidsL = find_aruco(calibImageL)
+ret, centroidsR = find_aruco(calibImageR)
+RVec, T = get_R_T_from_aruco(centroidsL, centroidsR)
 object3d.set_ground(RVec)
 
 lastUpdate = time.time()
 
 while True:
 
-    image = camera.get_image()
-    imageUndist = camera.get_image_undistort()
-    ret, corners, ids = find_aruco(image, numNeeded=1)
+    imageL, imageR = camera.get_images()
+    ret, cornersL = find_aruco(imageL)
+    ret1, cornersR = find_aruco(imageR)
     # ret, corners = find_chessboard_corners(image)
 
-    if ret:
+    if ret and ret1:
         # ret, RVec, T = cv2.solvePnP(chessPoints, corners, camera.cameraMatrix,
         #                             camera.distortion)
         # RVec, T = get_R_T_from_aruco(corners)
-        RVec, T = get_R_T_from_single_aruco(corners)
+        RVec, T = get_R_T_from_aruco(cornersL, cornersR)
         # graphy = get_homography_from_single_aruco(corners)
 
-        imageUndist = object3d.draw_object_on_image(imageUndist, camera, RVec,
-                                                    T)
-        print(T)
+        centroidPuckL = locate_puck(imageL)
+        centroidPuckR = locate_puck(imageR)
+        if (centroidPuckL is not None) and (centroidPuckR is not None):
+            puckCamera = puck_to_camera(centroidPuckL, centroidPuckR)
+        else:
+            puckCamera = None
+
+        imageUndist = object3d.draw_object_on_image(imageL, camera, RVec, T)
 
         dt = lastUpdate - time.time()
         lastUpdate = time.time()
-        object3d.update(RVec, dt)
+        object3d.update(RVec, T, puckCamera, dt)
 
     flipped = np.flip(imageUndist, axis=1)
     cv2.imshow("camera", flipped)
 
-    cv2.waitKey(30)
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+        break
 
 camera.release_camera()
 
